f7113a23f3e7233902922224fdd105603b269a2b
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-01-16 20:17:46 +0600

    style:hero skeleton added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import Container from '@/components/Container';
+import { Hero, HeroSubtitle, HeroTitle } from '@/components/Hero';
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div>
-      <p>test</p>
+      <header>
+        <Container>header</Container>
+      </header>
+      <main>
+        <Container>
+          <Hero>
+            <HeroTitle>
+              Linear is a better way <br /> to build products
+            </HeroTitle>
+            <HeroSubtitle>
+              Meet the new standard for modern software development. <br />{' '}
+              Streamline issues, sprints, and product road maps.
+            </HeroSubtitle>
+            <Image
+              src="/hero.webp"
+              alt="Hero Image"
+              width={1200}
+              height={900}
+            />
+          </Hero>
+        </Container>
+      </main>
+      <footer>
+        <Container>footer</Container>
+      </footer>
     </div>
   );
 }
---
+++ components/Container.tsx
@@ -0,0 +1,5 @@
+function Container({ children }: { children: React.ReactNode }) {
+  return <div className="max-w-[120rem] mx-auto">{children}</div>;
+}
+
+export default Container;
