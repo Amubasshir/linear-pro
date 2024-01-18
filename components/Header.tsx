271ed968cfe1c38007fe0f92eef0f5356d5eef9f
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-01-18 12:42:58 +0600

    cta added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
+import { Button } from './Buttons';
 import Container from './Container';
 import { Logo } from './icons/logo';
 
 function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full border-b border-transparent-white">
+    <header className="fixed top-0 left-0 w-full border-b border-transparent-white backdrop-blur-[12px]">
       <Container className="flex h-[var(--navigation-height)]">
         <Link href="/" className="flex items-center text-lg font-semibold">
           <Logo className="w-[1.8rem] h-[1.8rem] mr-2" /> Linear
         </Link>
         <nav className="h-full">
-          <ul className="flex items-center pt-[2px] h-full [&_a]:text-sm [&_li]:ml-6">
+          <ul className="flex items-center pt-[2px] h-full [&_a]:text-sm [&_li]:ml-6 [&_a:hover]:text-grey [&_a]:transition-colors">
             <li>
               <Link href="#">Features</Link>
             </li>
@@ -35,12 +36,10 @@ function Header() {
           </ul>
         </nav>
         <div className="ml-auto flex items-center h-full">
-          <Link className="text-sm ml-6" href="/">
+          <Link className="text-sm mr-6" href="/">
             Log in
           </Link>
-          <Link className="text-sm ml-6" href="/">
-            Sign up
-          </Link>
+          <Button href="/">Sign up</Button>
         </div>
       </Container>
     </header>
